cef29fc07889d40c3fd2fe32f421db8138a88231
akmalkhoerudin <akmalkhoer158@gmail.com>
2020-05-31 21:01:52 +0700

    rev1

--- js/favorit.js
+++ js/favorit.js
@@ -1,17 +1,26 @@
 // Tampil Data Club Favorit
 const clubFav = data => {
   let clubFavHtml = `
-      <li class="collection-header purple darken-4 white-text">
-        <h4>Data Club Favorite </h4>
+    <li class="collection-header purple darken-4 white-text">
+      <h4>Data Club Favorite </h4>
+    </li>
+  `;
+
+  if (data.length === 0) {
+    clubFavHtml += `
+      <li class="collection-item left-align">
+        <div>Club Favorite Tidak Ada.</div>
       </li>
     `;
-  data.forEach(club => {
-    clubFavHtml += `
-        <li class="collection-item left-align">
-          <div>${club.name}<a href="./detail-club.html?id=${club.id}" class="secondary-content"><i class="material-icons">arrow_forward</i></a></div>
-        </li>
-      `;
-  });
+  } else {
+    data.forEach(club => {
+      clubFavHtml += `
+          <li class="collection-item left-align">
+            <div>${club.name}<a href="./detail-club.html?id=${club.id}" class="secondary-content"><i class="material-icons">arrow_forward</i></a></div>
+          </li>
+        `;
+    });
+  }
   document.getElementById("preloader").style.display = 'none';
   document.getElementById("club-favorit").innerHTML = clubFavHtml;
 }
@@ -23,13 +32,22 @@ const playerFav = data => {
       <h4>Data Player Favorite </h4>
     </li>
   `;
-  data.forEach(pemain => {
+
+  if (data.length === 0) {
     playerFavHtml += `
       <li class="collection-item left-align">
-        <div>${pemain.name}<a href="./detail-pemain.html?id=${pemain.id}" class="secondary-content"><i class="material-icons">arrow_forward</i></a></div>
+        <div>Player Favorite Tidak Ada.</div>
       </li>
     `;
-  });
+  } else {
+    data.forEach(pemain => {
+      playerFavHtml += `
+        <li class="collection-item left-align">
+          <div>${pemain.name}<a href="./detail-pemain.html?id=${pemain.id}" class="secondary-content"><i class="material-icons">arrow_forward</i></a></div>
+        </li>
+      `;
+    });
+  }
   document.getElementById("preloader").style.display = 'none';
   document.getElementById("player-favorit").innerHTML = playerFavHtml;
 }
